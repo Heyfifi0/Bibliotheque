b0eb604317688b3c582edd10d174acb5a6625f51
Heyfifi0 <thomaschevalierpro@gmail.com>
2024-03-15 19:00:55 +0100

    fix éditeurs
    ils étaient pas dans l'ordre

--- app/Http/Controllers/EditeurController.php
+++ app/Http/Controllers/EditeurController.php
@@ -12,7 +12,7 @@ class EditeurController extends Controller
      */
     public function index()
     {
-        $editeurs = Editeur::all();
+        $editeurs = Editeur::orderBy('id_editeur')->get();
 
         return view('editeurs.index', compact('editeurs'));
     }
