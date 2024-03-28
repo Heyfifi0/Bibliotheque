24730dac4f6818e79ed27304b4e7aa9eea430318
Lywen <martinngeniee@gmail.com>
2024-03-28 12:04:19 +0100

    ajout gras

--- resources/views/abonnements/index.blade.php
+++ resources/views/abonnements/index.blade.php
@@ -33,11 +33,11 @@
                 <tbody class="text-gray-600 text-sm font-light">
                     @foreach ($abonnements as $abonnement)
                         <tr class="border-b border-gray-200 hover:bg-gray-100">
-                            <td class="py-3 px-6 text-left">{{ $abonnement->id_abonnement }}</td>
-                            <td class="py-3 px-6 text-left">{{ $abonnement->utilisateur->nom }} {{ $abonnement->utilisateur->prenom }}</td>
-                            <td class="py-3 px-6 text-left">{{ $abonnement->typeAbonnement->nom }}</td>
-                            <td class="py-3 px-6 text-left">{{ $abonnement->date_debut }}</td>
-                            <td class="py-3 px-6 text-left">{{ $abonnement->date_fin }}</td>
+                            <td class="py-3 px-6 text-left font-semibold">{{ $abonnement->id_abonnement }}</td>
+                            <td class="py-3 px-6 text-left font-semibold">{{ $abonnement->utilisateur->nom }} {{ $abonnement->utilisateur->prenom }}</td>
+                            <td class="py-3 px-6 text-left font-semibold">{{ $abonnement->typeAbonnement->nom }}</td>
+                            <td class="py-3 px-6 text-left font-semibold">{{ $abonnement->date_debut }}</td>
+                            <td class="py-3 px-6 text-left font-semibold">{{ $abonnement->date_fin }}</td>
                             <td class="py-3 px-6 text-left flex">
                                 <a href="{{ route('abonnements.edit', $abonnement) }}" class="text-gray-500 hover:text-gray-700 mr-4" data-toggle="modal">
                                     <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zm18.71-9.04a1 1 0 0 0 0-1.42l-2.34-2.34a1 1 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z"/></svg>
--- resources/views/type_abonnements/index.blade.php
+++ resources/views/type_abonnements/index.blade.php
@@ -28,9 +28,9 @@
                 <tbody class="text-gray-600 text-sm font-light">
                     @foreach ($type_abonnements as $type_abonnement)
                         <tr class="border-b border-gray-200 hover:bg-gray-100">
-                            <td class="py-3 px-6 text-left">{{ $type_abonnement->id_type_abonnement }}</td>
-                            <td class="py-3 px-6 text-left">{{ $type_abonnement->nom }}</td>
-                            <td class="py-3 px-6 text-left">{{ $type_abonnement->prix }}</td>
+                            <td class="py-3 px-6 text-left font-semibold">{{ $type_abonnement->id_type_abonnement }}</td>
+                            <td class="py-3 px-6 text-left font-semibold">{{ $type_abonnement->nom }}</td>
+                            <td class="py-3 px-6 text-left font-semibold">{{ $type_abonnement->prix }}</td>
 							<td class="py-3 px-6 text-left flex">
                                 <a href="{{ route('type_abonnements.edit', $type_abonnement) }}" class="text-gray-500 hover:text-gray-700 mr-4" data-toggle="modal">
                                     <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zm18.71-9.04a1 1 0 0 0 0-1.42l-2.34-2.34a1 1 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z"/></svg>
