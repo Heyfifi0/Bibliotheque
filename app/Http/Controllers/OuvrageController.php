516c4c0e9852375e50e363164b347e8adb7490b5
Frosperino <152378203+Frosperino@users.noreply.github.com>
2024-04-02 10:52:49 +0200

    Version finale genre

--- app/Http/Controllers/OuvrageController.php
+++ app/Http/Controllers/OuvrageController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ouvrage;
+use Illuminate\Pagination\Paginator;
 use App\Models\Genre;
+use App\Models\Auteur;
+use App\Models\Editeur;
+
+
 use Illuminate\Http\Request;
 
 class OuvrageController extends Controller
@@ -13,7 +18,8 @@ class OuvrageController extends Controller
      */
     public function index()
     {
-        //
+        $livres = Ouvrage::paginate(6); // Affiche 10 ouvrages par page
+        return view('ouvrages.indexT', compact('livres'));
     }
 
     /**
@@ -21,15 +27,41 @@ class OuvrageController extends Controller
      */
     public function create()
     {
-        //
-    }
+        $auteurs = Auteur::all(); // Récupère tous les auteurs
+        $genres = Genre::all(); // Récupère tous les genres
+        $editeurs = Editeur::all(); // Récupère tous les genres
+
+        return view('ouvrages.create', compact('auteurs', 'genres', 'editeurs'));    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        /*$request->validate([
+            'titre' => 'required|max:255',
+            'type' => 'required|max:255',
+            'id_editeur' => 'required|max:255',
+            'id_auteur' => 'required|array',
+            'id_genre' => 'required|array',
+
+
+        ]);*/
+        $ouvrage = new Ouvrage([
+            'titre' => $request->get('titre'),
+            'type' => $request->get('type'),
+            'id_editeur' => $request->get('editeur'),
+
+        ]);
+        $ouvrage->save();
+
+        // Ajout des auteurs à la table pivot
+        $ouvrage->auteurs()->attach($request->get('auteur'));
+
+        // Ajout des genres à la table pivot
+        $ouvrage->genres()->attach($request->get('genre'));
+    
+        return redirect('/ouvrages')->with('success','Ouvrage créé avec succès');
     }
 
     /**
@@ -46,22 +78,49 @@ class OuvrageController extends Controller
      */
     public function edit(Ouvrage $ouvrage)
     {
-        //
-    }
+        $auteurs = Auteur::all(); // Récupère tous les auteurs
+        $genres = Genre::all(); // Récupère tous les genres
+        $editeurs = Editeur::all(); // Récupère tous les genres
+        return view('ouvrages.edit', compact('ouvrage','auteurs', 'genres', 'editeurs'));
+      }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified resource {{ $livre->titre}}  in storage.
      */
-    public function update(Request $request, Ouvrage $ouvrage)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+           // 'titre' => 'required|max:255',
+        ]);
+
+        $ouvrage = Ouvrage::findOrFail($id);
+
+        $ouvrage->titre = $request->get('titre');
+
+        $ouvrage->type = $request->get('type');
+        $ouvrage->id_editeur = $request->get('editeur');
+
+        // Ajout des auteurs à la table pivot
+        $ouvrage->auteurs()->sync($request->get('auteur'));
+
+        // Ajout des genres à la table pivot
+        $ouvrage->genres()->sync($request->get('genre'));
+
+        $ouvrage->save();
+
+        // Redirect with success message
+        return redirect()->route('ouvrages.index')->with('success', 'Ouvrage mis à jour avec succès');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Ouvrage $ouvrage)
+    public function destroy($id)
     {
-        //
-    }
+        $livre = Ouvrage::findOrFail($id);
+        $livre->auteurs()->detach();
+        $livre->genres()->detach();
+        $livre->delete();
+    
+        return redirect('/ouvrages')->with('success', 'Livre supprimé avec succès');    }
 }
--- app/Models/Ouvrage.php
+++ app/Models/Ouvrage.php
@@ -21,23 +21,22 @@ class Ouvrage extends Model
         'titre',
         'type'
     ];
-    // avec type ENUM('livre','magazine','ebook')
-    
+
     public function auteurs(){
-        return $this->belongsToMany(Auteur::class);
+        return $this->belongsToMany(Auteur::class,'auteur_ouvrages', 'id_auteur', 'id_ouvrage');
     }
 
     public function commentaires(){
         return $this->belongsToMany(Commentaire::class);
     }
 
-    
+
     public function genres(){
         return $this->belongsToMany(Genre::class,'genre_ouvrages','id_ouvrage','id_genre');
     }
-    
+
     public function editeurs(){
-        return $this->belongsTo(Editeur::class);
+        return $this->belongsTo(Editeur::class, 'id_editeur');
     }
-    
+
 }
